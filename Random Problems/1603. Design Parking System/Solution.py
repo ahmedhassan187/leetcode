d22470d8dcffe512529f012daf3b95ee2f15aab0
AhmedHassan178 <87389435+AhmedHassan178@users.noreply.github.com>
2023-05-29 11:56:16 +0200

    add new random solution

---
+++ Random Problems/1603. Design Parking System/Solution.py
@@ -0,0 +1,25 @@
+class ParkingSystem:
+
+    def __init__(self, big: int, medium: int, small: int):
+        self.big = big
+        self.mid = medium
+        self.small = small
+    def addCar(self, carType: int) -> bool:
+        if carType == 1:
+            if self.big > 0:
+                self.big -= 1
+                return True
+            else:
+                return False
+        elif carType == 2:
+            if self.mid > 0:
+                self.mid -= 1
+                return True
+            else:
+                return False
+        elif carType == 3:
+            if self.small> 0:
+                self.small -= 1
+                return True
+            else:
+                return False
